869ae672ee77edd5b06b16b8ddc13fb85e188ace
ochoi12 <ochoi@wpi.edu>
2026-04-09 08:53:03 -0400

    olivia fixed it !

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
+
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
 import 'features/auth/presentation/auth_gate.dart';
 
-class SmallBizManagerApp extends StatelessWidget {
-  const SmallBizManagerApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Small Biz Manager',
-      theme: AppTheme.light(),
-      home: const AuthGate(),
-      onGenerateRoute: AppRouter.onGenerateRoute,
-      debugShowCheckedModeBanner: false,
-    );
-  }
+class SmallBizManagerApp extends StatelessWidget {
+ const SmallBizManagerApp({super.key});
+
+
+ @override
+ Widget build(BuildContext context) {
+   return MaterialApp(
+     title: 'Small Biz Manager',
+     theme: AppTheme.light(),
+     home: const AuthGate(),
+     onGenerateRoute: AppRouter.onGenerateRoute,
+     debugShowCheckedModeBanner: false,
+   );
+ }
 }
