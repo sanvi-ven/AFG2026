bff71046ede3014fc9ba918c3578d1603c33fb43
Sanvi Venkayala <svenkayala1@wpi.edu>
2026-04-10 17:41:55 -0400

    again

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -1,22 +1,29 @@
 import 'package:flutter/material.dart';
 
 
+
+
 import 'core/router/app_router.dart';
 import 'core/theme/app_theme.dart';
+import 'features/auth/presentation/login_page.dart';
+
+
 
 
 class SmallBizManagerApp extends StatelessWidget {
- const SmallBizManagerApp({super.key});
+const SmallBizManagerApp({super.key});
 
 
- @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     title: 'Small Biz Manager',
-     theme: AppTheme.light(),
-     initialRoute: AppRouter.login,
-     onGenerateRoute: AppRouter.onGenerateRoute,
-     debugShowCheckedModeBanner: false,
-   );
- }
+
+
+@override
+Widget build(BuildContext context) {
+  return MaterialApp(
+    title: 'Anchor',
+    theme: AppTheme.light(),
+    home: const LoginPage(),
+    onGenerateRoute: AppRouter.onGenerateRoute,
+    debugShowCheckedModeBanner: false,
+  );
+}
 }
